a88244878e9d433a983c8905bf7facf5a06b5c42
chadwick <ch@dw.ck>
2023-01-27 23:03:04 +0100

    sqlite and moving authorisation out of file

--- udemy_courses.py
+++ udemy_courses.py
@@ -1,9 +1,20 @@
-"""
-getting a courses list in a single python list
+#!/usr/bin/env python
+# coding: utf-8
 
-https://www.udemy.com/developers/affiliate/methods/get-courses-list/
-"""
+import cred
+
 import requests as rq
+import sqlite3
+
+dbc = sqlite3.connect("udemy_classes.db")
+dbr = dbc.cursor()
+dbr.execute(
+    """create table if not exists udemy_classes(
+    key integer,
+    title text,
+    price test)
+    """)
+dbc.commit()
 
 all_classes = list()
 page: int = 0
@@ -14,17 +25,39 @@ udemy_url_endpoint: str = "https://www.udemy.com/api-2.0"
 udemy_url_classes: str = f"courses/?page={page}"
 headers: dict = {
   "Accept": "application/json, text/plain, */*",
-  "Authorization": "Basic R0JNb2ZFMHVKUVgwM20yeWhrVFp5R3ZYOUhrOGxndHduYmxUQnE3NTptczZxVzZiNkgyWVNTNUY0MTlJbXVRMkF4aFVIS2c4VXpJMmZZMWdoaENnaGlHZFFkSzA3V211WXQ4Y21uUFVkVWptdlExREs3VlFZdk9FUzdRMjYyTEJ3WHAxQnBDYnJYUmlZMWFPdTdiOEw5TW1ia0FFRVAxbnR5SmtmV2NYMA==",
+  "Authorization": cred.authorisation,
   "Content-Type": "application/json"
 }
 
 while page < 10:
-    classes_response = rq.get(f"{udemy_url_endpoint}/{udemy_url_classes}", headers=headers)
+    classes_response = rq.get(
+        f"{udemy_url_endpoint}/{udemy_url_classes}",
+        headers=headers)
     classes_json = classes_response.json()
-    if 'results' in classes_json:
-        all_classes.extend(classes_json['results'])
+    if "results" in classes_json:
+        all_classes.extend(classes_json["results"])
     page += 1
     udemy_url_classes = f"courses/?page={page}"
     print(f"page: {page}")
 
 print(f"len all_classes: {len(all_classes)}")
+
+for i, all_class in enumerate(all_classes):
+    dbr.execute(
+        "insert into udemy_classes values (?, ?, ?)",
+        (i, all_class["title"], all_class["price"])
+    )
+
+    print(
+        f"{i} class\n"
+        f"TITLE: {all_class['title']}\n"
+        f"PRICE: {all_class['price']}\n"
+        "####\n"
+    )
+
+dbc.commit()
+
+nonfree = dbr.execute("select * from udemy_classes where price != 'Free'")
+nonfree_courses = nonfree.fetchall()
+for nonfree_course in nonfree_courses:
+    print(f"nonfreecourse: {nonfree_course}")
